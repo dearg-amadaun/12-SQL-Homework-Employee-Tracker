9207ac20ffc93f3d2c2e957a900c8b1d877773e2
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-23 17:14:23 -0400

    Employee tables, index.js start

---
+++ index.js
@@ -0,0 +1,22 @@
+//Dependencies
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'rootr00t!',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
---
+++ sql/schema.sql
@@ -0,0 +1,33 @@
+--Create database
+DROP DATABASE IF EXISTS emnployees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+-- Creates the table "department" within employees_db and increments the id --
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+--Creates the roles table--
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(19,4) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+--Creates the employee table--
+CREATE TABLE employee (
+    id IN NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
+); 
+
+
