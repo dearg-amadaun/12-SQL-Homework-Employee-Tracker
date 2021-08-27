ddd432dadb73a1cd52145b7c632f49f7514a699f
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-27 17:44:39 -0400

    fixed sql tables

--- db/schema.sql
+++ db/schema.sql
@@ -10,23 +10,22 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary INT NOT NULL,
     department_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) 
-    REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL, 
-    role_id INT,
-    manager_id INT,
     PRIMARY KEY (id),
+    role_id INT, 
     FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT,
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
--- db/seed.sql
+++ db/seed.sql
@@ -1,27 +1,24 @@
-USEemployees_db 
-
-INSERT INTO department(department_name)
-VALUES("Engineering"), 
-      ("Sales"), 
-      ("Finance"), 
-      ("Legal"), 
-      ("Marketing");
+INSERT INTO department (name)
+VALUES ("Engineering"), 
+       ("Sales"), 
+       ("Finance"), 
+       ("Legal");
 
 
-INSERT INTO role(title, role_id, department, salary)
-VALUES("Sales Lead", 1, "Sales", 100000), 
-      ("Salesperson" 2, "Sales", 80000), 
-      ("Lead Engineer", 3, "Engineering", 150000), 
-      ("Software Engineer", 4, "Engineering", 120000), 
-      ("Account Manager", 5, "Finance", 160000), 
-      ("Accountant", 6, "Finance", 125000), 
-      ("Legal Team Lead", 7, "Legal", 250000), 
-      ("Lawyer", 8, "Legal", 190000);
+INSERT INTO role (title, id, department_id, salary)
+VALUES ("Sales Lead", 1, 1, 100000),
+       ("Salesperson", 2, 1, 80000), 
+       ("Lead Engineer", 3, 2, 150000), 
+       ("Software Engineer", 4, 2, 120000), 
+       ("Account Manager", 5, 3, 160000), 
+       ("Accountant", 6, 3, 125000),
+       ("Legal Team Lead", 7, 4, 250000),
+       ("Lawyer", 8, 4, 190000);
 
 
 
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, null), 
        ("Mike", "Chan", 2, 1), 
        ("Ashley", "Rodriguez", 3, null), 
@@ -29,6 +26,6 @@ VALUES ("John", "Doe", 1, null),
        ("Kunal", "Singh", 5, null), 
        ("Malia", "Brown", 6, 5), 
        ("Sarah", "Lourd", 7, null), 
-       ("John", "Allen", 8, 7);
+       ("Jeff", "Allen", 8, 7);
 
 
