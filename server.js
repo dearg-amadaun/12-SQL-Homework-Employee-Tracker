e1308da447517389d21ffcd54bc1d0e411f35958
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-30 12:10:31 -0400

    building out api calls

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require("express");
-const cTable = require('console.table');
+const employeeDatabase = require("./db/employees_db");
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({extende: false}))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json());
+
+app.get("/api/department", (req, res) => {
+    employeeDatabase.selectAllDepartments().then((data) => {
+        console.log(data);
+    })
+});
+
+app.get("/api/role", (req, res) => {
+    employeeDatabase.selectAllRoles().then((data) => {
+        console.log(data);
+    })
+});
+
+app.get("/api/employee", (req, res) => {
+    employeeDatabase.selectAllEmployees().then((data) => {
+        console.log(data);
+    })
+});
+
+app.listen(PORT, () => console.log(`RUNNING ON ${PORT}!`));
